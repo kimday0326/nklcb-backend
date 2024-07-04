3c2c92558b3f286275734be2d62ca8e57e8315df
kimday0326 <ktnqkr01@gmail.com>
2024-07-04 17:01:56 +0900

    feat: client:client-grpc 모듈 추가

---
+++ client/client-grpc/src/test/java/com/hwi/client/grpc/ClientGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwi.client.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
