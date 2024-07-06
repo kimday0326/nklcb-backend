12f22c8ae9991facc5d5fd01cae65a0a67c2a693
kimday0326 <ktnqkr01@gmail.com>
2024-07-07 03:26:57 +0900

    chore: 기본 data.sql 추가

---
+++ storage/db-core/src/main/resources/db/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO nklcb.company (name, rss_url, created_at, updated_at)
+VALUES ('NAVER D2', 'https://d2.naver.com/d2.atom', NOW(), NOW()),
+       ('Kakao Enterprise', 'https://tech.kakaoenterprise.com/feed', NOW(), NOW()),
+       ('LINE', 'https://engineering.linecorp.com/ko/feed/index.html', NOW(), NOW()),
+       ('Coupang', 'https://medium.com/feed/coupang-engineering', NOW(), NOW()),
+       ('Woowa Brothers', 'https://techblog.woowahan.com/feed', NOW(), NOW());
