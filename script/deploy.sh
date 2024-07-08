cb892ada5a023263195daf4ec4a0b2cb5cb7cb0d
kimday0326 <ktnqkr01@gmail.com>
2024-07-08 18:44:47 +0900

    cd: delploy.sh 변경

--- script/deploy.sh
+++ script/deploy.sh
@@ -8,36 +8,36 @@ if [ -z $IS_GREEN  ];then # blue인 경우
   echo "### BLUE => GREEN ###"
 
   echo "1. get green image"
-  docker-compose pull green # green으로 이미지를 내려받습니다.
+  docker compose pull green # green으로 이미지를 내려받습니다.
 
   echo "2. green container up"
-  docker-compose up -d green # green 컨테이너 실행
+  docker compose up -d green # green 컨테이너 실행
 
   while [ 1 = 1 ]; do
   echo "3. green health check..."
   sleep 3
 
   REQUEST=$(curl http://127.0.0.1:8081) # green으로 request
-    if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
-            echo "health check success"
-            break ;
-            fi
+      if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
+              echo "health check success"
+              break ;
+              fi
   done;
 
   echo "4. reload nginx"
   sudo cp /etc/nginx/nginx.green.conf /etc/nginx/nginx.conf
-  sudo nginx -s rel
+  sudo nginx -s reload
 
   echo "5. blue container down"
-  docker-compose stop blue
+  docker compose stop blue
 else
   echo "### GREEN => BLUE ###"
 
   echo "1. get blue image"
-  docker-compose pull blue
+  docker compose pull blue
 
   echo "2. blue container up"
-  docker-compose up -d blue
+  docker compose up -d blue
 
   while [ 1 = 1 ]; do
     echo "3. blue health check..."
@@ -55,5 +55,5 @@ else
   sudo nginx -s reload
 
   echo "5. green container down"
-  docker-compose stop green
+  docker compose stop green
 fi
